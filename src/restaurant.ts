8d243ecfeea26b132be98f6f6d863f1754c230e7
ValentinaCifuentes <v.cifuentes04@ufromail.cl>
2026-08-28 17:51:09 -0400

    feat: implement minimum crearReserva to return a reservation object

--- src/restaurant.ts
+++ src/restaurant.ts
@@ -1,17 +1,25 @@
 export class Restaurant {
     reservations: Reserva[] = [];
 
+    constructor(private capacidadMaxima: number) {}
+
+    crearReserva(datos: {
+        nombreCliente: string;
+        cantidadPersonas: number;
+        fecha: string;
+        hora: string;
+    }): Reserva {
+        return new Reserva('1', datos.nombreCliente, datos.cantidadPersonas, datos.fecha, datos.hora);
+    }
 }
 
 
 class Reserva {
     constructor(
-        public id: string,
-        public customers: Customer[],
-        public date: string
+        public codigoReserva: string,
+        public nombreCliente: string,
+        public cantidadPersonas: number,
+        public fecha: string,
+        public hora: string
     ) { }
-}
-
-class Customer {
-    constructor(public id: string, public name: string) { }
 }
